658908d4875ea5dd67b24442ea80d93e5fe1960e
Artur Nikitsin <32310512+artur-nikitsin@users.noreply.github.com>
2025-07-25 22:53:58 +0400

    test commit

--- src/components/pages/page/PagePreview.tsx
+++ src/components/pages/page/PagePreview.tsx
@@ -18,6 +18,7 @@ export default function PagePreview(props: Props) {
   const { data } = useQuery<PagePayload | null>(pagesBySlugQuery, params, {
     initial,
   })
+  return null;
 
   return <Page data={data!} />
 }
